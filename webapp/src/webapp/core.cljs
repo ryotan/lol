e942628e36fd7713f0d9473859114d2db586a70d
ryotan <ryotan@ucb-jp.net>
2015-03-02 01:22:56 +0900

    Contacts CRD.

--- webapp/src/webapp/core.cljs
+++ webapp/src/webapp/core.cljs
@@ -1,6 +1,11 @@
 (ns ^:figwheel-always webapp.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [figwheel.client :as fw]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -8,14 +13,77 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello OM!"}))
+(defonce app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-(om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
 
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c      (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+(defn add-contact [data owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                      .-value
+                      parse-contact)]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact))
+      (om/set-state! owner :text ""))))
+
+(defn contact-view [contact owner]
+  (reify om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+        (dom/span nil (display-name contact))
+        (dom/button #js {:onClick #(put! delete @contact)} "Delete")))))
+
+(defn handle-change [e owner {:keys [text]}]
+  (om/set-state! owner :text (.. e -target -value)))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)
+       :text   ""})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+              (let [contact (<! delete)]
+                (om/transact! data :contacts
+                  (fn [xs] (vec (remove #(= contact %) xs))))
+                (recur))))))
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+        (dom/h2 nil "Contact List")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts data)
+            {:init-state state}))
+        (dom/div nil
+          (dom/input #js {:type     "text" :ref "new-contact" :value (:text state)
+                          :onChange #(handle-change % owner state)})
+          (dom/button #js {:onClick #(add-contact data owner)} "Add Contact"))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
